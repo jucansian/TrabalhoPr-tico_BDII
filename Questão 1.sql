4d68e39044854238651c3746ba6fdfcd0e6b6ac4
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-06 14:42:03 -0300

    Update Questão 1.sql

--- Questão 1.sql
+++ Questão 1.sql
@@ -1,1 +1,12 @@
--- SELECT m.nome, count(c.id) as qtde_consultas from medico m, consulta c where m.id = c.id_medico and c.data_hora_inicio > sysdate() group by m.nome order by m.nome;
+-- PRIMEIRA VERSÃO:
+SELECT m.nome, count(c.id) as qtde_consultas from medico m, consulta c where m.id = c.id_medico and c.data_hora_inicio > sysdate() group by m.nome order by m.nome;
+
+-- VERSÃO OTIMIZADA:
+CREATE INDEX idx_id_medico ON consulta(id_medico);
+
+SELECT m.nome, count(c.id) as qtde_consultas FROM medico m
+  JOIN consulta c ON m.id = c.id_medico
+  WHERE c.data_hora_inicio > sysdate()
+  GROUP BY m.nome
+  HAVING qtde_consultas > 0
+  ORDER BY m.nome;
