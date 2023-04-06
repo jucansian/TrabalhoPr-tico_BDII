ecae38d5c477b8eb037f0f18cde3d9a0cce26374
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-06 14:43:50 -0300

    Update Questão 2.sql

--- Questão 2.sql
+++ Questão 2.sql
@@ -1,1 +1,27 @@
--- SELECT e.nome, count(c.id) as qtde_consultas from especialidade e, medico m, consulta c where e.id = m.id_especialidade and m.id = c.id_medico group by e.nome;
+-- PRIMEIRA VERSÃO:
+SELECT e.nome, count(c.id) as qtde_consultas from especialidade e, medico m, consulta c where e.id = m.id_especialidade and m.id = c.id_medico group by e.nome;
+
+-- VERSÃO OTIMIZADA:
+ALTER TABLE medico ADD INDEX idx_id_especialidade (id_especialidade);
+ALTER TABLE consulta ADD INDEX idx_id_medico (id_medico);
+
+SELECT e.nome, COUNT(c.id) as qtde_consultas
+  FROM especialidade e
+  INNER JOIN medico m ON e.id = m.id_especialidade
+  INNER JOIN consulta c ON m.id = c.id_medico
+  GROUP BY e.nome
+  HAVING MAX(c.data_hora_inicio) > NOW();
+
+SELECT e.nome, COUNT(c.id) AS qtde_consultas
+  FROM especialidade e
+  INNER JOIN medico m ON e.id = m.id_especialidade
+  INNER JOIN consulta c ON m.id = c.id_medico
+  WHERE c.data_hora_inicio > NOW()
+  GROUP BY e.nome;
+
+EXPLAIN SELECT e.nome, COUNT(c.id) AS qtde_consultas
+  FROM especialidade e
+  INNER JOIN medico m ON e.id = m.id_especialidade
+  INNER JOIN consulta c ON m.id = c.id_medico
+  WHERE c.data_hora_inicio > NOW()
+  GROUP BY e.nome;
