3e27391f9887687db317d39b257f99704349ecd1
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-06 14:50:18 -0300

    Update Questão 4.sql

--- Questão 4.sql
+++ Questão 4.sql
@@ -1,1 +1,30 @@
--- SELECT m.nome, count(c.id) as consultas from medico m, especialidade e, consulta c where m.id = c.id_medico and e.id = m.id_especialidade and c.data_hora_fim < DATE_ADD(sysdate(), INTERVAL 300 minute) group by m.id order by consultas desc LIMIT 1;
+-- PRIMEIRA VERSÃO:
+SELECT m.nome, count(c.id) AS consultas 
+  FROM medico m, especialidade e, consulta c 
+  WHERE m.id = c.id_medico 
+  AND e.id = m.id_especialidade 
+  AND c.data_hora_fim < DATE_ADD(sysdate(), INTERVAL 300 minute) 
+  GROUP BY m.id 
+  ORDER BY consultas desc LIMIT 1;
+  
+-- VERSÃO OTIMIZADA:
+CREATE INDEX idx_data_hora_fim ON consulta (data_hora_fim);
+  
+SELECT m.nome, COUNT(c.id) AS consultas
+  FROM medico m
+  JOIN consulta c ON m.id = c.id_medico
+  JOIN especialidade e ON m.id_especialidade = e.id
+  WHERE c.data_hora_fim < DATE_ADD(sysdate(), INTERVAL 300 MINUTE)
+  GROUP BY m.id
+  ORDER BY consultas DESC
+  LIMIT 1;
+  
+EXPLAIN SELECT m.nome, COUNT(c.id) AS consultas
+  FROM medico m
+  JOIN consulta c ON m.id = c.id_medico
+  JOIN especialidade e ON m.id_especialidade = e.id
+  WHERE c.data_hora_fim < DATE_ADD(sysdate(), INTERVAL 300 MINUTE)
+  GROUP BY m.id
+  ORDER BY consultas DESC
+  LIMIT 1;
+  
