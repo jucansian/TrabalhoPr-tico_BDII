c296d05af9ae8eacf664ad928d07215c64e77e75
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-06 14:52:34 -0300

    Update Questão 5.sql

--- Questão 5.sql
+++ Questão 5.sql
@@ -1,1 +1,22 @@
--- SELECT p.nome as paciente, m.nome as medico, e.nome as especialidade, c.data_hora_inicio from consulta c, medico m, paciente p, especialidade e where c.id_medico = m.id and c.id_paciente = p.id and m.id_especialidade = e.id
+-- PRIMEIRA VERSÃO:
+SELECT p.nome AS paciente, m.nome AS medico, e.nome AS especialidade, c.data_hora_inicio 
+  FROM consulta c, medico m, paciente p, especialidade e
+  WHERE c.id_medico = m.id 
+  AND c.id_paciente = p.id
+  AND m.id_especialidade = e.id;
+  
+-- VERSÃO OTIMIZADA:
+CREATE INDEX idx_consulta_id_medico ON consulta (id_medico);
+CREATE INDEX idx_medico_id ON medico (id);
+
+SELECT p.nome as paciente, m.nome as medico, e.nome as especialidade, c.data_hora_inicio
+  FROM consulta c
+  JOIN medico m ON c.id_medico = m.id
+  JOIN paciente p ON c.id_paciente = p.id
+  JOIN especialidade e ON m.id_especialidade = e.id;
+  
+EXPLAIN SELECT p.nome as paciente, m.nome as medico, e.nome as especialidade, c.data_hora_inicio
+  FROM consulta c
+  JOIN medico m ON c.id_medico = m.id
+  JOIN paciente p ON c.id_paciente = p.id
+  JOIN especialidade e ON m.id_especialidade = e.id;
