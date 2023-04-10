b01e7fb4f24e75c7c4461925605032d7024c8603
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-10 20:53:22 -0300

    Update Questão 3.sql

--- Questão 3.sql
+++ Questão 3.sql
@@ -3,13 +3,10 @@ SELECT DATE_FORMAT(c.data_hora_inicio, '%d/%m'), (MAX(TIMESTAMPDIFF(MINUTE, c.da
   FROM CONSULTA c 
   GROUP BY DATE_FORMAT(c.data_hora_inicio, '%d/%m');
 
--- VERSÃO OTIMIZADA:
+-- VERSÃO OTIMIZADA 1:
 CREATE INDEX ix_consulta_data_hora_inicio ON CONSULTA (data_hora_inicio);
 
-SELECT DATE_FORMAT(c.data_hora_inicio, '%d/%m') AS data_consulta, (MAX(TIMESTAMPDIFF(MINUTE, c.data_hora_inicio, c.data_hora_fim))) AS maior_duracao
+EXPLAIN SELECT DATE_FORMAT(c.data_hora_inicio, '%d/%m') AS data_consulta, (MAX(TIMESTAMPDIFF(MINUTE, c.data_hora_inicio, c.data_hora_fim))) AS maior_duracao
   FROM CONSULTA c
   GROUP BY DATE_FORMAT(c.data_hora_inicio, '%d/%m');
   
-EXPLAIN SELECT DATE_FORMAT(c.data_hora_inicio, '%d/%m') AS data_consulta, MAX(TIMESTAMPDIFF(MINUTE, c.data_hora_inicio, c.data_hora_fim)) AS maior_duracao
-  FROM CONSULTA c
-  GROUP BY DATE_FORMAT(c.data_hora_inicio, '%d/%m');
