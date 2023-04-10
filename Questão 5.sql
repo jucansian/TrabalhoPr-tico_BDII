4f38ac397fbca182ee7c9ab26928c1b67f15f085
jucansian <110699596+jucansian@users.noreply.github.com>
2023-04-10 20:58:18 -0300

    Update Questão 5.sql

--- Questão 5.sql
+++ Questão 5.sql
@@ -9,14 +9,9 @@ SELECT p.nome AS paciente, m.nome AS medico, e.nome AS especialidade, c.data_hor
 CREATE INDEX idx_consulta_id_medico ON consulta (id_medico);
 CREATE INDEX idx_medico_id ON medico (id);
 
-SELECT p.nome as paciente, m.nome as medico, e.nome as especialidade, c.data_hora_inicio
-  FROM consulta c
-  JOIN medico m ON c.id_medico = m.id
-  JOIN paciente p ON c.id_paciente = p.id
-  JOIN especialidade e ON m.id_especialidade = e.id;
-  
 EXPLAIN SELECT p.nome as paciente, m.nome as medico, e.nome as especialidade, c.data_hora_inicio
   FROM consulta c
   JOIN medico m ON c.id_medico = m.id
   JOIN paciente p ON c.id_paciente = p.id
   JOIN especialidade e ON m.id_especialidade = e.id;
+
